2cb258cc977eeb78e1e04129e5299779188063cf
valesskablanco <valesskablanco2@gmail.com>
2021-11-15 19:54:27 -0600

    Inicios de la pantalla adicional

--- scr/Main/MainClass.java
+++ scr/Main/MainClass.java
@@ -1,4 +1,14 @@
 package Main;
 
+import Interface.About;
+
 public class MainClass {
+
+    //prueba de GUI
+
+    public static void main(String[] args) {
+
+        About about = new About();
+
+    }
 }
