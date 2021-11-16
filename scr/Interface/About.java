7bf99bbe72742cce6ca28c4293d390bb54737784
valesskablanco <valesskablanco2@gmail.com>
2021-11-15 19:52:46 -0600

    Inicios de la pantalla adicional

--- scr/Interface/About.java
+++ scr/Interface/About.java
@@ -1,5 +1,45 @@
 package Interface;
 
-public class About {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class About extends JFrame implements KeyListener {
+
+    // es necesario crear una clase button para hacer el manejo de el ActionPerformed
+
+    JPanel pane;
+    JTextField search;
+
+    public About() {
+
+        this.setTitle("Información Adicional");
+        this.setVisible(true);
+        this.setSize(400,600);
+
+        this.pane = new JPanel();
+        this.getContentPane().add(this.pane);
+        this.pane.setLayout(null);
+        this.pane.setBackground(Color.decode("#fff9eb"));
+        this.setResizable(false);
+
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
